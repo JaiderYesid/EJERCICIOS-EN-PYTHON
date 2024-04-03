3a6d2442b639bfc2a4f1910ea1f19b4de273595a
JaiderYesid <150965073+JaiderYesid@users.noreply.github.com>
2024-04-03 10:38:08 -0500

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,8 @@
+#Programa que permita calcular la edad de una persona conociendo el año actual y su año de nacimiento
+
+año_nacimiento = int(input("Ingrese el año de nacimiento: "))
+año_actual = 2024 
+
+edad = año_actual - año_nacimiento
+
+print("La edad es:", edad, "años")
