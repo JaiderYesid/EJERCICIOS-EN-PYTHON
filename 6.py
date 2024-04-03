9ad87e93281871bfb35d6a9315df63a7ccb0e551
JaiderYesid <150965073+JaiderYesid@users.noreply.github.com>
2024-04-03 10:45:21 -0500

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,11 @@
+#Programa que permita calcular el 30%, el 60% y el 90% de un número cualquiera
+
+numero = float(input("Ingrese el número: "))
+
+treinta_porciento = numero * 0.30
+sesenta_porciento = numero * 0.60
+noventa_porciento = numero * 0.90
+
+print(f"El 30% de {numero} es: {treinta_porciento}")
+print(f"El 60% de {numero} es: {sesenta_porciento}")
+print(f"El 90% de {numero} es: {noventa_porciento}")
