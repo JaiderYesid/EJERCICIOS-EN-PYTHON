1702daf8c3247f333d0abaa0e831fdff1b7e0f86
JaiderYesid <150965073+JaiderYesid@users.noreply.github.com>
2024-04-03 10:36:42 -0500

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,7 @@
+# Programa que permita multiplicar 3 números
+
+valor1 = int(input("Primer número: "))
+valor2 = int(input("Segundo número: "))
+valor3 = int(input("Tercer número: "))
+
+print("Suma es igual a: ", valor1 * valor2 * valor3)
