c096d7984f6ebc23694690b572f7fc2307df93ab
JaiderYesid <150965073+JaiderYesid@users.noreply.github.com>
2024-04-03 10:47:18 -0500

    Create 7.py

---
+++ 7.py
@@ -0,0 +1,6 @@
+#Programa para calcular el área de un cuadrado
+
+lado = float(input("Ingrese la longitud del lado del cuadrado: "))
+area = lado * lado
+
+print(f"El área del cuadrado con lado {lado} es: {area}")
