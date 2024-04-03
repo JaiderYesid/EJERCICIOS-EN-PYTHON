4cbcedbc5416c4ec8c9e00915d91744e7350e3b8
JaiderYesid <150965073+JaiderYesid@users.noreply.github.com>
2024-04-03 10:42:50 -0500

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,6 @@
+#Programa para calcular el 20% de cualquier número de entrada
+
+numero = float(input("Ingrese el número: "))
+porcentaje = numero * 0.20
+
+print(f"El 20% de {numero} es: {porcentaje}")
