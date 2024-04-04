36dbc50dc5a7aaf64dc6025aa413951d97b18871
JaiderYesid <150965073+JaiderYesid@users.noreply.github.com>
2024-04-04 18:24:13 -0500

    Update 66.py

--- 66.py
+++ 66.py
@@ -1,4 +1,4 @@
-#Programa que permita llenar una matriz cuyo número de filas y columnas es ingresado por el usuario y luego calcule la suma de cada una de sus filas (una x una)
+#Programa que permita llenar una matriz cuyo número de filas y columnas es ingresado por el usuario, donde se busque un valor (Ingresado por el usuario) y al encontrarlo muestre su posición (fila, columna). 
 
 num_filas = int(input("Ingrese el número de filas de la matriz: "))
 num_columnas = int(input("Ingrese el número de columnas de la matriz: "))
@@ -7,14 +7,18 @@ matriz = []
 for _ in range(num_filas):
     fila = []
     for _ in range(num_columnas):
-        valor = float(input(f"Ingrese un valor para la posición ({len(matriz)+1}, {len(fila)+1}): "))
+        valor = int(input(f"Ingrese un valor para la posición ({len(matriz)+1}, {len(fila)+1}): "))
         fila.append(valor)
     matriz.append(fila)
 
-suma_filas = []
-for fila in matriz:
-    suma_fila = sum(fila)
-    suma_filas.append(suma_fila)
+valor_buscar = int(input("Ingrese el valor que desea buscar en la matriz: "))
 
-for i, suma in enumerate(suma_filas):
-    print(f"La suma de la fila {i+1} es: {suma}")
+encontrado = False
+for i in range(num_filas):
+    for j in range(num_columnas):
+        if matriz[i][j] == valor_buscar:
+            print(f"El valor {valor_buscar} se encuentra en la posición ({i+1}, {j+1})")
+            encontrado = True
+
+if not encontrado:
+    print(f"El valor {valor_buscar} no se encuentra en la matriz.")
